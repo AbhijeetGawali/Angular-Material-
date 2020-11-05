a5f043549ff4cd9d889a3f7cef066f98f7f78e37
Abhijeet Gawali <abhgawali@yahoo.com>
2020-11-05 15:52:40 +0530

    Add New material Tabs, Stepper , List , Expansion Panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,17 @@ import { TypographyComponent } from './typography/typography.component';
 import { ButtonComponent } from './button/button.component';
 import { IconsComponent } from './icons/icons.component';
 import { BadgeComponent } from './badge/badge.component';
+import { ProgressSpinnerComponent } from './progress-spinner/progress-spinner.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MenuComponent } from './menu/menu.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { ListComponent } from './list/list.component';
+import { GridListComponent} from './grid-list/grid-list.component';
+import { ExpansionPanelComponent} from './expansion-panel/expansion-panel.component';
+import { CardsComponent} from './cards/cards.component';
+import { TabsComponent}  from './tabs/tabs.component';
+import { StepperComponent } from './stepper/stepper.component';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +31,17 @@ import { BadgeComponent } from './badge/badge.component';
     TypographyComponent,
     ButtonComponent,
     IconsComponent,
-    BadgeComponent
+    BadgeComponent,
+    ProgressSpinnerComponent,
+    ToolbarComponent,
+    MenuComponent,
+    SidenavComponent,
+    ListComponent,
+    GridListComponent,
+    ExpansionPanelComponent,
+    CardsComponent,
+    TabsComponent,
+    StepperComponent
   ],
   imports: [
     BrowserModule,
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,13 +1,37 @@
-import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatBadgeModule} from '@angular/material/badge'
+import { NgModule} from '@angular/core';
 
+// Below is Material Module
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatMenuModule} from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCardModule} from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatStepperModule} from '@angular/material/stepper';
 
 const MaterialComponents=
 [MatButtonModule,
   MatIconModule,
-  MatBadgeModule
+  MatBadgeModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatMenuModule,
+  MatListModule,
+  MatDividerModule,
+  MatGridListModule,
+  MatExpansionModule,
+  MatCardModule,
+  MatTabsModule,
+  MatStepperModule
+
 ]
 
 @NgModule({
